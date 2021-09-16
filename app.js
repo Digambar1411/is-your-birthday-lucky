afe0e5fe400e0d2ad1ba7dee21d67d919d5a8f67
Digambar <deshawaldigambar@gmail.com>
2021-09-16 20:28:50 +0530

    input and console the output on click

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ var bDate=document.querySelector("#bdate");
 console.log(bDate);
 var luckyNum= document.querySelector("#luckynum");
 console.log(luckyNum);
+var checkBtn= document.querySelector("#check-btn");
+
+checkBtn.addEventListener("click", calculateLucky );
+
+function calculateLucky(){
+    console.log("clicke");
+}
+
+
 
 
