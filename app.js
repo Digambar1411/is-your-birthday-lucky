a84059491179a9a761b8c6a1fbd32ca905ebf825
Digambar <deshawaldigambar@gmail.com>
2021-09-17 18:01:30 +0530

    completes app
    All HTML, CSS, and Javascript part completed

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var bDate=document.querySelector("#bdate");
-console.log(bDate);
 var luckyNum= document.querySelector("#luckynum");
-console.log(luckyNum);
 var checkBtn= document.querySelector("#check-btn");
-
+var result=document.querySelector("#is-lucky");
 checkBtn.addEventListener("click", calculateLucky );
 
+alert("Note : we are not storing any data")
+
 function calculateLucky(){
-    console.log("clicke");
+    var birthDate = bDate.value;
+    var arr = birthDate.replaceAll("-","").split(""); /* return array of string */
+    sumOfArr(arr);
+    
 }
 
+function sumOfArr(arr){
+    var sum=0;
+    for (var i=0; i<arr.length; i++){
+        sum+=Number(arr[i]);
+        if(sum%luckyNum.value===0){
 
+            result.innerText="Wow your birthday is lucky !!! 🎉";
+        }
+        else{
+            result.innerText="Oops !!! your birthday is not lucky 😞";
 
+        }
+        
+    }
+
+}
 
