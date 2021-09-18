5402bbec298bf1970f637b412cca40a1e665de4d
Digambar <deshawaldigambar@gmail.com>
2021-09-18 12:36:10 +0530

    feat: added error message
    added function to show error message

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bDate=document.querySelector("#bdate");
 var luckyNum= document.querySelector("#luckynum");
+var errorMsg=document.querySelector("#error-msg");
 var checkBtn= document.querySelector("#check-btn");
 var result=document.querySelector("#is-lucky");
 checkBtn.addEventListener("click", calculateLucky );
@@ -7,6 +8,7 @@ checkBtn.addEventListener("click", calculateLucky );
 alert("Note : we are not storing any data")
 
 function calculateLucky(){
+    hideMessage();
     var birthDate = bDate.value;
     var arr = birthDate.replaceAll("-","").split(""); /* return array of string */
     sumOfArr(arr);
@@ -14,19 +16,35 @@ function calculateLucky(){
 }
 
 function sumOfArr(arr){
+    // hideMessage();
     var sum=0;
-    for (var i=0; i<arr.length; i++){
-        sum+=Number(arr[i]);
-        if(sum%luckyNum.value===0){
+    if(bDate.value && luckyNum.value){
+        for (var i=0; i<arr.length; i++){
+            sum+=Number(arr[i]);
+            if(sum%luckyNum.value===0){
 
-            result.innerText="Wow your birthday is lucky !!! 🎉";
-        }
-        else{
-            result.innerText="Oops !!! your birthday is not lucky 😞";
-
+                result.innerText="Wow your birthday is lucky !!! 🎉";
+            }
+            else{
+                result.innerText="Oops !!! your birthday is not lucky 😞";
+            }
         }
         
     }
+    else{
+        showMessage("date of birth or lucky number cannot be null");
+    }
 
 }
+
+function showMessage(msg){
+    errorMsg.innerText=msg;
+    errorMsg.style.display="block";
+
+}
+
+function hideMessage(){
+    errorMsg.style.display="none";
+}
+
 
